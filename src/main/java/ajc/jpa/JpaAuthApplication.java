6f974ddc62b059c9a74729f8960321764e3899e5
ElmerTheFarmer <kosalexandre@gmmail.com>
2023-02-10 14:46:58 +0100

    First commit

---
+++ src/main/java/ajc/jpa/JpaAuthApplication.java
@@ -0,0 +1,13 @@
+package ajc.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajc/jpa/JpaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ajc.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
